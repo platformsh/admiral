81a3a4a0d972fab52e526e32a109e25dc06b806e
Larry Garfield <larry@garfieldtech.com>
2019-08-05 16:05:48 -0500

    Set necessary environment variables on a newly created project.

--- src/EventListener/DoctrineProject.php
+++ src/EventListener/DoctrineProject.php
@@ -6,7 +6,6 @@ namespace App\EventListener;
 use App\Entity\Project;
 use App\PlatformClient;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
-use Doctrine\ORM\Events;
 
 class DoctrineProject
 {
@@ -46,5 +45,30 @@ class DoctrineProject
         // Record the subscription's project ID in the local project record.
         // Now in the future we can load the project data on the fly.
         $project->setProjectId($subscription->project_id);
+    }
+
+    /**
+     * Acts on an object just after it has been saved.
+     *
+     * @param LifecycleEventArgs $args
+     */
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        /** @var Project $project */
+        $project = $args->getObject();
+
+        // This event listener triggers for all saved documents, but we care only about Projects.
+        if (! $project instanceof Project) {
+            return;
+        }
+
+        // Now that the project has been created on Platform.sh, set its environment
+        // variables based on the project Archetype.  These will be needed by the source operations.
+        $archetype = $project->getArchetype();
+        $pshProject = $this->client->getProject($project->getProjectId());
+
+        $pshProject->setVariable('env:UPDATE_REMOTE', $archetype->getGitUri());
+        $pshProject->setVariable('env:UPDATE_BRANCH', $archetype->getUpdateBranch());
+        $pshProject->setVariable('env:UPDATE_OPERATION', $archetype->getUpdateOperation());
     }
 }
