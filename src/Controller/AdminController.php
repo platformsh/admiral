44f8eae2032dd29a9220762397de0bfad21361fa
Larry Garfield <larry@garfieldtech.com>
2019-08-06 17:34:48 -0500

    Add skeleton of custom controllers to support the custom commands we need to write.

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+
+class AdminController extends EasyAdminController
+{
+
+}
