e07049295fc18bdfdca9fc7d9242c7a7917d60c5
Larry Garfield <larry@garfieldtech.com>
2019-08-09 13:23:04 -0500

    Add web profiler toolbar for easier development.

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
