427d8d14de0d4c9ddbe626cae478790f5e2ccdc1
Larry Garfield <larry@garfieldtech.com>
2019-08-12 13:48:52 -0500

    Don't fatal on a missing project or archetype when updating variables.

--- src/MessageHandler/SetProjectVariablesHandler.php
+++ src/MessageHandler/SetProjectVariablesHandler.php
@@ -7,6 +7,7 @@ use App\Entity\Archetype;
 use App\Message\SetProjectVariables;
 use App\PlatformClient;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 /**
@@ -28,10 +29,16 @@ class SetProjectVariablesHandler implements MessageHandlerInterface
      */
     protected $em;
 
-    public function __construct(PlatformClient $client, EntityManagerInterface $em)
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct(PlatformClient $client, EntityManagerInterface $em, LoggerInterface $logger)
     {
         $this->client = $client;
         $this->em = $em;
+        $this->logger = $logger;
     }
 
     /**
@@ -42,11 +49,28 @@ class SetProjectVariablesHandler implements MessageHandlerInterface
     public function __invoke(SetProjectVariables $message)
     {
         $archetype = $this->em->getRepository(Archetype::class)->find($message->getArchetypeId());
+        if (is_null($archetype)) {
+            // This means the archetype was deleted between when this command was requested
+            // and now. Log it and move on since there's not much else we can do.
+            $this->logger->error('Cannot update project {pshProjectId} from archetype {archetypeId}. The archetype is missing.', [
+                'pshProjectId' => $message->getPshProjectId(),
+                'archetypeId' => $message->getArchetypeId(),
+            ]);
+            return;
+        }
         $pshProject = $this->client->getProject($message->getPshProjectId());
+        if (!$pshProject) {
+            // This means the project was deleted between when this command was requested
+            // and now. Log it and move on since there's not much else we can do.
+            $this->logger->error('Cannot update project {pshProjectId} from archetype {archetypeId}. The project does not exist.', [
+                'pshProjectId' => $message->getPshProjectId(),
+                'archetypeId' => $message->getArchetypeId(),
+            ]);
+            return;
+        }
 
         $pshProject->setVariable('env:UPDATE_REMOTE', $archetype->getGitUri());
         $pshProject->setVariable('env:UPDATE_BRANCH', $archetype->getUpdateBranch());
         $pshProject->setVariable('env:UPDATE_OPERATION', $archetype->getUpdateOperation());
-
     }
 }
