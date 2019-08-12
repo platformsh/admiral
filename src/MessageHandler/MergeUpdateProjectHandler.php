a47af465429a92a2d5fd635f19f7d2563b5ba36b
Larry Garfield <larry@garfieldtech.com>
2019-08-12 13:38:03 -0500

    Show useful error without a crash when merging a missing project.

--- src/MessageHandler/MergeUpdateProjectHandler.php
+++ src/MessageHandler/MergeUpdateProjectHandler.php
@@ -9,6 +9,7 @@ use App\PlatformClient;
 use Doctrine\ORM\EntityManagerInterface;
 use Platformsh\Client\Model\Environment;
 use Platformsh\Client\Model\Project as PshProject;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 /**
@@ -34,10 +35,16 @@ class MergeUpdateProjectHandler implements MessageHandlerInterface
      */
     protected $em;
 
-    public function __construct(PlatformClient $client, EntityManagerInterface $em)
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct(PlatformClient $client, EntityManagerInterface $em, LoggerInterface $logger)
     {
         $this->client = $client;
         $this->em = $em;
+        $this->logger = $logger;
     }
 
     /**
@@ -48,7 +55,24 @@ class MergeUpdateProjectHandler implements MessageHandlerInterface
     public function __invoke(MergeUpdateProject $message)
     {
         $project = $this->em->getRepository(Project::class)->find($message->getProjectId());
+        if (is_null($project)) {
+            // This means the project was deleted sometime between when the merge was requested
+            // and now.  If that's the case then just log it and forget about it, since there's
+            // not much else to do.
+            $this->logger->error('Merge requested for project {id}, but that project no longer exists.', [
+                'id' => $message->getProjectId()
+            ]);
+            return;
+        }
+
         $pshProject = $this->client->getProject($project->getProjectId());
+        if (!$pshProject) {
+            $this->logger->error('Platform.sh project {pshProjectId} not found for project {title}', [
+                'pshProjectId' => $project->getProjectId(),
+                'title' => $project->getTitle()
+            ]);
+            return;
+        }
         $archetype = $project->getArchetype();
 
         if ($env = $this->getActiveEnvironment($pshProject, $archetype->getUpdateBranch())) {
