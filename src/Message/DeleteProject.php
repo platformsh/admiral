e516d123a79cf9f89849afa3d39d0253d0e6e6bf
Larry Garfield <larry@garfieldtech.com>
2019-08-14 11:58:56 -0500

    Document why deletion is different.

--- src/Message/DeleteProject.php
+++ src/Message/DeleteProject.php
@@ -3,6 +3,13 @@ declare(strict_types=1);
 
 namespace App\Message;
 
+/**
+ * Message to request deletion of a project.
+ *
+ * Because the local Project is already deleted at this point,
+ * we must pass the Platform.sh Project ID forward.  The local Project ID
+ * is no longer available.
+ */
 class DeleteProject
 {
     /**
